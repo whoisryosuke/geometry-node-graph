11a3f88f4a5c49156b2bad44d02ec907bac83063
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-08-01 15:03:08 -0700

    :wrench: updates for build process

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve("src", "index.ts"),
+      name: "ReactGeometryNodeGraph",
+      formats: ["es", "umd"],
+      fileName: (format) => `react-geometry-node-graph.${format}.js`,
+    },
+  },
+});
