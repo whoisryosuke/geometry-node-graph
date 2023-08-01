37f7b04300f3ba40898d5b0bbf36a877999a5c0f
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-08-01 16:45:46 -0700

    :construction: stil not exporting properly

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve("src", "index.ts"),
-      name: "ReactGeometryNodeGraph",
+      name,
       formats: ["es", "umd"],
       fileName: (format) => `${name}.${format}.js`,
     },
