7bc33a69eff1a49db7987cff9ca47326e9d2ab5e
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-08-01 10:17:28 -0700

    :sparkles: mvp kinda

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import { useState } from 'react'
 import ReactFlow, { Controls, Background } from 'reactflow';
 import 'reactflow/dist/style.css';
-
-
+import data from "./data/Test-nodes-53.json"
 import 'reactflow/dist/style.css';
+
+
+// Convert Blender format to ReactFlow
+const nodes = data.nodes.map(node => ({
+  id: node.uuid,
+  position: node.location,
+  data: {
+    label: node.name
+  }
+}))
+
+const edges = data.links.map(link => {
+  const hash = Number(new Date).toString(36)
+
+  return {
+    id: hash,
+    source: link.from_node,
+    target: link.to_node,
+  }
+})
 
 const initialNodes = [
   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
@@ -15,7 +34,7 @@ function App() {
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges}>
+      <ReactFlow nodes={nodes} edges={edges}>
         <Background />
         <Controls />
       </ReactFlow>
